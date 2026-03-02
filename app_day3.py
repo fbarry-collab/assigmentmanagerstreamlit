d399f6699efc7d9113aaddf8f37cbef082536152
fbarry-collab <fbarry@udel.edu>
2026-03-02 12:26:22 -0500

    Session Monday

---
+++ app_day3.py
@@ -0,0 +1,115 @@
+import streamlit as st
+import time 
+import json
+from pathlib import Path 
+
+st.set_page_config(page_title="Course Management", page_icon="", layout="centered", initial_sidebar_state="collapsed")
+
+
+st.title ("Course Management App")
+st.divider()
+
+next_assignemnt_id=3
+
+#load data
+assignments = [
+    {
+        "id": "HW1",
+        "title" : "Intro to database",
+        "description": "basics of database design",
+        "points" : 100, 
+        "type": "homework"
+    } ,
+    { 
+        "id": "HW2",
+        "title": "Normalization",
+        "description": "normalizing",
+        "points" : 100,
+        "type": "homework"
+
+    }
+]
+
+json_path=("assignments.json")
+
+#Load the data from a json file
+if json_path.exists():
+    with json_path.open("r", encoding="utf-8") as f:
+        assignments=json.load(f)
+
+tab1, tab2, tab3 =st.tabs(["View Assigments", "Add New Assignemnt", "Update an Assignment"])
+
+with tab1:
+    tab_option=st.radio ("View/Search", ["View", "Search"], horizontal=True)
+    if tab_option =="View":
+         st.dataframe(assignments)
+    else:
+        titles = []
+    for assignment in assignments:
+        titles.append(assignment["title"])
+
+    selected_title= st.selectbox("Select a Title", titles, key ="selected_title")
+
+    selected_assignment= {}
+    for assignment in assignments:
+        if assignment["title"]== selected_title:
+            selected_assignment = assignment
+            break
+
+    if not selected_assignment:
+        with st.expander("Assignment Details", expanded= True):
+            st.markdown(f"### Title: {selected_assignment["title"]}")
+            st.markdown(f"Description: {selected_assignment["description"]}")
+            st.markdown(f"Type: **{selected_assignment["type"]}**")
+
+with tab2:
+    st.markdown("## Add New Assignment")
+#st.markdown("### Add New Assignment")
+
+    title = st.text_input("Title")
+    description = st.text_area("Description", placeholder="normalization is covered here",
+                            help="Here you are entering the assignment details")
+    points = st.number_input("Points")
+
+#assignment_type=st.text_input("Assignment Type")
+    assignment_type=st.radio("Type", ["Homework", "Lab"], horizontal= True)
+    st.caption("Homework Type")
+
+assignment_type2=st.selectbox("Type", ["Select an Option","Homework", "Lab", "other"])
+if assignment_type2=="other":
+    assignment_type2=st.text_input("Type",placeholder="Enter the assignment type")
+
+due_date =st.date_input("Due Date")
+
+btn_save=st.button("Save", width="stretch", disabled=False)
+
+with tab3:
+    st.markdown("## Update an Assignment")
+    titles =[]
+    for assignment in assignments:
+        titles.append(assignment["title"])
+
+    selected_item= st.selectbox("Select an Assignment", titles, key="selected_title_edit")
+
+    assignment_edit ={}
+    for assignment in assignments:
+        if assignment["title"] == selected_item:
+            assignment_edit = assignment
+            break
+
+    if assignment_edit:
+        edit_title = st.text_input ("Title", key= "edit_title", value= assignment_edit["title"])
+        edit_description = st.text_area ("Description", key= "edit_description", value=assignment_edit["description"])
+
+    btn_update=st.button ("Update", key="Update_button", type="secondary", use_container_width= True)
+    if btn_update:
+        with st.spinner("Updating..."):
+            time.sleep(5)
+            assignment_edit["title"]= edit_title
+            assignment_edit["description"]= edit_description
+
+            with json_path.open("w", encoding= "utf-8") as f:
+                json.dump(assignments, f)
+            st.success("Updated...")
+
+
